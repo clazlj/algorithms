df5fb0c22bc8965af1bc3c7fb42eca2be4277331
clazlj <584574197@qq.com>
2020-09-18 00:57:16 +0800

    0

---
+++ exam/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package exam;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class AddTwoNumbers {
+    private static void addTwoNumbers(ListNode tempNode, ListNode l1, ListNode l2, int todoAdd) {
+        if (l1 == null && l2 == null) {
+            return;
+        }
+        int l1Val = l1 == null ? 0 : l1.val;
+        int l2Val = l2 == null ? 0 : l2.val;
+        int sum = l1Val + l2Val + todoAdd;
+        int remainder = sum % 10;
+        todoAdd = sum >= 10 ? 1 : 0;
+
+        tempNode.next = new ListNode(remainder);
+        addTwoNumbers(tempNode.next, l1.next, l2.next, todoAdd);
+    }
+
+    // TODO: 2020/9/18 0018 修复(5) + (5)
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int sum = l1.val + l2.val ;
+        int remainder = sum % 10;
+        int todoAdd = sum >= 10 ? 1 : 0;
+        ListNode resultNode = new ListNode(remainder);
+        addTwoNumbers(resultNode, l1.next, l2.next, todoAdd);
+
+        return resultNode;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO: 2020/9/18 0018  
+    }
+}
